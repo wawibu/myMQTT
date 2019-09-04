e28eebf6a67b5f228def3decbf70caa4c9873cfc
wawibu <32385342+wawibu@users.noreply.github.com>
2019-09-04 22:00:58 +0200

    Add files via upload

---
+++ TG_M1/module.php
@@ -0,0 +1,79 @@
+<?php
+
+class myMQTT
+{
+    public function Create()
+    {
+        //Never delete this line!
+        parent::Create();
+        $this->BufferResponse = '';
+        $this->ConnectParent('{C6D2AEB3-6E1F-4B2E-8E69-3A1A00246850}');
+        //Anzahl die in der Konfirgurationsform angezeigt wird - Hier Standard auf 1
+        $this->RegisterPropertyString('Topic', '');
+    }
+
+    public function ApplyChanges()
+    {
+        //Never delete this line!
+        parent::ApplyChanges();
+        $this->BufferResponse = '';
+        $this->ConnectParent('{C6D2AEB3-6E1F-4B2E-8E69-3A1A00246850}');
+        //Setze Filter fÃ¼r ReceiveData
+
+        $this->SendDebug(__FUNCTION__ . ' FullTopic', $this->ReadPropertyString('FullTopic'), 0);
+        $topic = $this->FilterFullTopicReceiveData();
+        $this->SendDebug(__FUNCTION__ . ' Filter FullTopic', $topic, 0);
+
+        $this->SetReceiveDataFilter('.*' . $topic . '.*');
+    }
+
+    public function ReceiveData($JSONString)
+    {
+        $this->SendDebug('JSON', $JSONString, 0);
+        if (!empty($this->ReadPropertyString('Topic'))) {
+            $this->SendDebug('ReceiveData JSON', $JSONString, 0);
+            $data = json_decode($JSONString);
+            // Buffer decodieren und in eine Variable schreiben
+            $Buffer = $data;
+            $this->SendDebug('Topic', $Buffer->Topic, 0);
+
+            //IrReceived
+            if (fnmatch('*IrReceived*', $Buffer->Payload)) {
+                $myBuffer = json_decode($Buffer->Payload);
+                $this->SendDebug('IrReceived', $Buffer->Payload, 0);
+                if (property_exists($myBuffer->IrReceived, 'Protocol')) {
+                    $this->RegisterVariableString('Tasmota_IRProtocol', 'IR Protocol', '', 0);
+                    SetValue($this->GetIDForIdent('Tasmota_IRProtocol'), $myBuffer->IrReceived->Protocol);
+                }
+                if (property_exists($myBuffer->IrReceived, 'Bits')) {
+                    $this->RegisterVariableString('Tasmota_IRBits', 'IR Bits', '', 0);
+                    SetValue($this->GetIDForIdent('Tasmota_IRBits'), $myBuffer->IrReceived->Bits);
+                }
+                if (property_exists($myBuffer->IrReceived, 'Data')) {
+                    $this->RegisterVariableString('Tasmota_IRData', 'IR Data', '', 0);
+                    SetValue($this->GetIDForIdent('Tasmota_IRData'), $myBuffer->IrReceived->Data);
+                }
+            }
+
+        }
+    }
+
+    public function RequestAction($Ident, $Value)
+    {
+        $this->SendDebug(__FUNCTION__ . ' Ident', $Ident, 0);
+        $this->SendDebug(__FUNCTION__ . ' Value', $Value, 0);
+        if ($Ident == 'Tasmota_FanSpeed') {
+            $result = $this->setFanSpeed($Value);
+            return true;
+        }
+
+        if (strlen($Ident) != 13) {
+            $power = substr($Ident, 13);
+        } else {
+            $power = 0;
+        }
+        $result = $this->setPower($power, $Value);
+    }
+
+    
+}
